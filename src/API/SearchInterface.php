2f4b0e242ca919b048c3dec64ae78df59a6fad7b
Daniel Costa <danielcosta@gmail.com>
2016-02-12 01:54:34 -0200

    Added SearchInterface for the core API

---
+++ src/API/SearchInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jobles\Core\API;
+
+use Jobles\Core\Job\JobCollection;
+
+interface SearchInterface
+{
+    /**
+     * @param array $filters
+     * @return JobCollection
+     */
+    public function search(array $filters);
+}
