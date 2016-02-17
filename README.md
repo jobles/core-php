ecb28e4dc99a9c649d7d174c10bc016ea2318180
Daniel Costa <danielcosta@gmail.com>
2016-02-17 20:59:14 -0200

    Adding unit tests for Index\Index class
    - Also added README.bd with quality badges

---
+++ README.md
@@ -0,0 +1,8 @@
+# core-php
+
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/jobles/core-php/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/jobles/core-php/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/jobles/core-php/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/jobles/core-php/?branch=master)
+[![Build Status](https://scrutinizer-ci.com/g/jobles/core-php/badges/build.png?b=master)](https://scrutinizer-ci.com/g/jobles/core-php/build-status/master)
+
+[![Coverage Status](https://coveralls.io/repos/github/jobles/core-php/badge.svg?branch=master)](https://coveralls.io/github/jobles/core-php?branch=master)
+[![Build Status](https://travis-ci.org/jobles/core-php.svg?branch=master)](https://travis-ci.org/jobles/core-php)
---
+++ tests/Index/IndexTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jobles\Tests\Core\Index;
+
+use Jobles\Core\Index\Index;
+
+class IndexTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Index
+     */
+    private $index;
+
+    public function setUp()
+    {
+        $this->index = new Index('index_key', 'Index Name', 'http://index.url', 'affId');
+    }
+
+    public function testGetKey()
+    {
+        $this->assertEquals('index_key', $this->index->getKey());
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('Index Name', $this->index->getName());
+    }
+
+    public function testGetUrl()
+    {
+        $this->assertEquals('http://index.url', $this->index->getUrl());
+    }
+
+    public function testGetAffiliateId()
+    {
+        $this->assertEquals('affId', $this->index->getAffiliateId());
+    }
+}
