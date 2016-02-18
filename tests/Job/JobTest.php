e52a1e095293d38b0676202cc01f396fa094af4d
Daniel Costa <danielcosta@gmail.com>
2016-02-17 22:31:38 -0200

    Improved JobTest

--- tests/Job/JobTest.php
+++ tests/Job/JobTest.php
@@ -2,6 +2,7 @@
 
 namespace Jobles\Tests\Core\Job;
 
+use Jobles\Core\Index\Index;
 use Jobles\Core\Job\Job;
 
 class JobTest extends \PHPUnit_Framework_TestCase
@@ -15,11 +16,8 @@ class JobTest extends \PHPUnit_Framework_TestCase
     {
         $this->job = new Job;
         $this->job->setKey('job_key');
+        $this->job->setIndex(new Index('index_key', 'Index Name', 'http://index.url', 'affId'));
         $this->job->setTitle('Analista de Sistema para Software de RH');
-        $this->job->setDate(new \DateTime('Sat, 13 Feb 2016 08:59:39 GMT'));
-        $this->job->setSnippet('Principais atribuições do cargo...');
-        $this->job->setViewUrl('http://job.url/pt-br/job-12345');
-        $this->job->setSource('www.ceviu.com.br');
         $this->job->setCompany('Mega Enterprise, Co');
         $this->job->setSalaryCurrencyCode('BRL');
         $this->job->setSalaryMin(1000);
@@ -27,6 +25,13 @@ class JobTest extends \PHPUnit_Framework_TestCase
         $this->job->setCity('Sao Paulo');
         $this->job->setState('SP');
         $this->job->setCountry('Brasil');
+        $this->job->setSource('www.ceviu.com.br');
+        $this->job->setDate(new \DateTime('Sat, 13 Feb 2016 08:59:39 GMT'));
+        $this->job->setSnippet('Principais atribuições do cargo...');
+        $this->job->setDescription('Descrição detalhada do cargo...');
+        $this->job->setViewUrl('http://job.url/pt-br/job-12345');
+        $this->job->setApplyUrl('http://job.url/pt-br/apply-12345');
+        $this->job->setFeatured(false);
     }
 
     public function testGetKey()
@@ -34,29 +39,18 @@ class JobTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('job_key', $this->job->getKey());
     }
 
+    public function testGetIndex()
+    {
+        $this->assertInstanceOf(Index::class, $this->job->getIndex());
+        $this->assertEquals('index_key', $this->job->getIndex()->getKey());
+        $this->assertEquals('Index Name', $this->job->getIndex()->getName());
+        $this->assertEquals('http://index.url', $this->job->getIndex()->getUrl());
+        $this->assertEquals('affId', $this->job->getIndex()->getAffiliateId());
+    }
+
     public function testGetTitle()
     {
         $this->assertEquals('Analista de Sistema para Software de RH', $this->job->getTitle());
-    }
-
-    public function testGetDate()
-    {
-        $this->assertEquals('2016-02-13 08:59:39', $this->job->getDate()->format('Y-m-d H:i:s'));
-    }
-
-    public function testGetSnippet()
-    {
-        $this->assertEquals('Principais atribuições do cargo...', $this->job->getSnippet());
-    }
-
-    public function testGetViewUrl()
-    {
-        $this->assertEquals('http://job.url/pt-br/job-12345', $this->job->getViewUrl());
-    }
-
-    public function testGetSource()
-    {
-        $this->assertEquals('www.ceviu.com.br', $this->job->getSource());
     }
 
     public function testGetCompany()
@@ -92,5 +86,40 @@ class JobTest extends \PHPUnit_Framework_TestCase
     public function testGetCountry()
     {
         $this->assertEquals('Brasil', $this->job->getCountry());
+    }
+
+    public function testGetSource()
+    {
+        $this->assertEquals('www.ceviu.com.br', $this->job->getSource());
+    }
+
+    public function testGetDate()
+    {
+        $this->assertEquals('2016-02-13 08:59:39', $this->job->getDate()->format('Y-m-d H:i:s'));
+    }
+
+    public function testGetSnippet()
+    {
+        $this->assertEquals('Principais atribuições do cargo...', $this->job->getSnippet());
+    }
+
+    public function testGetDescription()
+    {
+        $this->assertEquals('Descrição detalhada do cargo...', $this->job->getDescription());
+    }
+
+    public function testGetViewUrl()
+    {
+        $this->assertEquals('http://job.url/pt-br/job-12345', $this->job->getViewUrl());
+    }
+
+    public function testGetApplyUrl()
+    {
+        $this->assertEquals('http://job.url/pt-br/apply-12345', $this->job->getApplyUrl());
+    }
+
+    public function testisFeatured()
+    {
+        $this->assertFalse($this->job->isFeatured());
     }
 }
